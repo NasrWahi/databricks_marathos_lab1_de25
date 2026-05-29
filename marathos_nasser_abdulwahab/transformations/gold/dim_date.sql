bfe771cc2bac647d212d10b7e9f84f7f0897a002
NasrWahi <nasr.wahi@gmail.com>
2026-05-29 14:53:02 +0000

    up dim_date

--- marathos_nasser_abdulwahab/transformations/gold/dim_date.sql
+++ marathos_nasser_abdulwahab/transformations/gold/dim_date.sql
@@ -2,3 +2,17 @@
 
 CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.dim_date
   COMMENT "Date dimension BONUS - gold layer" AS
+SELECT DISTINCT
+  DENSE_RANK() OVER (ORDER BY event_start_date)  AS date_id,
+  event_start_date                               AS full_date,
+  YEAR(event_start_date)                         AS year,
+  MONTH(event_start_date)                        AS month,
+  DAY(event_start_date)                          AS day,
+  DATE_FORMAT(event_start_date, 'EEEE')          AS weekday,
+  QUARTER(event_start_date)                      AS quarter
+FROM
+  marathos.silver.marathon_obt
+WHERE
+  event_start_date IS NOT NULL
+ORDER BY
+  date_id;
