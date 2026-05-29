db34ab9943256feda81a35bac0df5c527b77dbc5
NasrWahi <nasr.wahi@gmail.com>
2026-05-29 13:28:08 +0000

    used two files, fixed an error, should work

--- marathos_nasser_abdulwahab/transformations/gold/dim_event.sql
+++ marathos_nasser_abdulwahab/transformations/gold/dim_event.sql
@@ -8,3 +8,17 @@ WITH ranked_events AS (
       DENSE_RANK() OVER (ORDER BY event_name) AS event_id
     FROM (SELECT DISTINCT event_name FROM marathos.silver.marathon_obt)
 )
+SELECT
+  r.event_id,
+  r.event_name,
+  MAX_BY(s.event_distance_value, s.event_start_date)      AS event_distance_value,
+  MAX_BY(s.event_distance_unit, s.event_start_date)       AS event_distance_unit,
+  MAX_BY(s.event_type, s.event_start_date)                AS event_type,
+  MAX_BY(s.event_number_of_finishers, s.event_start_date) AS event_number_of_finishers
+FROM
+  marathos.silver.marathon_obt s
+  INNER JOIN ranked_events r ON s.event_name = r.event_name
+GROUP BY
+  r.event_id, r.event_name
+ORDER BY
+  r.event_id;
