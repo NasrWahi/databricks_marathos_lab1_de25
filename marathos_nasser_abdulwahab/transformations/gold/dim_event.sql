8f1a3f4aa674c6fdf7a03d76f5abc81fe8da0dfc
NasrWahi <nasr.wahi@gmail.com>
2026-06-04 11:27:36 +0000

    coalesce

--- marathos_nasser_abdulwahab/transformations/gold/dim_event.sql
+++ marathos_nasser_abdulwahab/transformations/gold/dim_event.sql
@@ -11,10 +11,10 @@ WITH ranked_events AS (
 SELECT
   r.event_id,
   r.event_name,
-  MAX_BY(s.event_distance_value, s.event_start_date)      AS event_distance_value,
-  MAX_BY(s.event_distance_unit, s.event_start_date)       AS event_distance_unit,
-  MAX_BY(s.event_type, s.event_start_date)                AS event_type,
-  MAX_BY(s.event_number_of_finishers, s.event_start_date) AS event_number_of_finishers
+  COALESCE(MAX_BY(s.event_distance_value, s.event_start_date), MAX(s.event_distance_value))           AS event_distance_value,
+  MAX(s.event_distance_unit)                                                                          AS event_distance_unit,
+  MAX(s.event_type)                                                                                   AS event_type,
+  COALESCE(MAX_BY(s.event_number_of_finishers, s.event_start_date), MAX(s.event_number_of_finishers)) AS event_number_of_finishers
 FROM
   marathos.silver.marathon_obt s
   INNER JOIN ranked_events r ON s.event_name = r.event_name
